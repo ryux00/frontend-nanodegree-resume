c4b69759d030a4ce7269d8027ed4621d31882e0c
Nishit Mohanan <Nishitmohanan@gmail.com>
2016-08-17 16:41:50 +0530

    Added name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append(["Nishit Mohanan"]);
+ // $("#main").append(["Nishit Mohanan "]);
+
+
+ // awesomeThoughts ="AWESOME";
+ // funThoughts=awesomeThoughts.replace("AWESOME","Fun");
+ //  $("#main").append([funThoughts]);
+
+
+ formattedName = HTMLheaderName.replace("%data%","Nishit Mohanan");
+ formattedRole = HTMLheaderRole.replace("%data%","Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
